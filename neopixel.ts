9b316a5eb883b73b3b170d72d5fc2cecc1934c3a
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2021-06-03 10:09:49 +0900

    更新 neopixel.ts

--- neopixel.ts
+++ neopixel.ts
@@ -103,7 +103,7 @@ export enum Colors {
      * Gets the RGB value of a known color
      * @param rgb RGB color of the LED, eg:Colors.Red
     */
-    //% block="color %color"
+    //% block="%color"
     export function colors(color: Colors): number {
         return color;
     }
